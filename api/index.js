9d47ce7e509df9096f67b12fefc7593b9fdc3cb1
Timofey <timofey.syr17@gmail.com>
2024-08-14 15:36:48 +0300

    RM2 - Create sendMessage api

--- api/index.js
+++ api/index.js
@@ -3,11 +3,15 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const sendMessageRoute = require('./routes/sendMessage')
+
 const app = express()
 const port = process.env.PORT || 4000
 
 app.use(cors())
 app.use(bodyParser.json())
+
+app.use('/send-message', sendMessageRoute)
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
