b71a26ad98b834b8f1d6cbe5a5c58fbb5f492c4e
Timofey <timofey.syr17@gmail.com>
2024-09-24 18:41:31 +0300

    next config fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL || 'http://185.251.38.139/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   images: {
     remotePatterns: [
