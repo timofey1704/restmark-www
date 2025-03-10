97149bc2ff784e82cf5e9e10bbf52d9fed06bfe8
Timofey <timofey.syr17@gmail.com>
2025-03-10 17:44:14 +0300

    urls fix

--- frontend/lib/fetchMain.ts
+++ frontend/lib/fetchMain.ts
@@ -1,8 +1,8 @@
 import { MainPageData } from '@/app/types'
 
 export async function fetchMain(): Promise<MainPageData> {
-  // const API_URL = 'https://restmark.by/api/v1'
-  const API_URL = 'http://127.0.0.1:8000/api/v1'
+  const API_URL = 'https://restmark.by/api/v1'
+  // const API_URL = 'http://127.0.0.1:8000/api/v1'
 
   const response = await fetch(`${API_URL}/main/`)
   if (!response.ok) {
--- frontend/lib/fetchSitemapLinks.ts
+++ frontend/lib/fetchSitemapLinks.ts
@@ -1,8 +1,8 @@
 import { Seolinks } from '@/app/types'
 
 export async function fetchSitemapLinks(): Promise<Seolinks[]> {
-  // const API_URL = 'https://restmark.by/api/v1'
-  const API_URL = 'http://127.0.0.1:8000/api/v1'
+  const API_URL = 'https://restmark.by/api/v1'
+  // const API_URL = 'http://127.0.0.1:8000/api/v1'
   try {
     const res = await fetch(`${API_URL}/sitemap/`)
 
